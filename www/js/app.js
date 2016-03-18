144c90e95e7da1b4a5f7c5d1ba9960f652a3cdf0
RuanAyram <ruan_kaylo@yahoo.com.br>
2016-03-18 16:57:27 -0300

    Arquivos

---
+++ www/js/app.js
@@ -0,0 +1,104 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'  'Prot_ServNow.map_controller'
+// 'starter.controllers' is found in controllers.js
+var app = angular.module('Prot_ServNow', ['ionic','ngMap', 'Prot_ServNow.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+app.controller('MyController', function(NgMap) {
+  NgMap.getMap().then(function(map) {
+    console.log(map.getCenter());
+    console.log('markers', map.markers);
+    console.log('shapes', map.shapes);
+  });
+})
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+  .state('app.inicial_mapa', {
+    url: '/inicial_mapa',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/inicial_mapa.html',
+        controller: 'MyController'
+      }
+    }
+  })
+  .state('app.Ler_codigo', {
+      url: '/Ler_codigo',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/Ler_codigo.html'
+        }
+      }
+    })
+  .state('app.cadastrar', {
+    url: '/cadastrar',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/cadastrar.html'
+      }
+    }
+  })
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/inicial_mapa');
+});
---
+++ www/js/map_controller.js
@@ -0,0 +1,9 @@
+/*angular.module('Prot_ServNow.map_controller', ['ngMap'])
+
+.controller('MyController', function(NgMap) {
+  NgMap.getMap().then(function(map) {
+    console.log(map.getCenter());
+    console.log('markers', map.markers);
+    console.log('shapes', map.shapes);
+  });
+})
